efd53aa2c401534101e63008312547327338a4bd
EJ-C <10ngweekendofficial@gmail.com>
2020-12-22 16:23:47 +0900

    fixed code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
-// const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
@@ -45,17 +44,9 @@ async function getQuote() {
   }
 }
 
-// Tweet Quote
-// function tweetQuote() {
-//     const quote = quoteText.innerText;
-//     const author = authorText.innerText;
-//     const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-//     window.open(twitterUrl, '_blank');
-// }
 
 // Event Listeners
 newQuoteBtn.addEventListener('click', getQuote);
-// twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
